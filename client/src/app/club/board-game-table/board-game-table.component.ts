5f7953833d9b8339b20252945e1596be160a7017
Andrew Polk <polkandrew01@gmail.com>
2024-11-06 15:41:21 -0800

    Add new BoardGame child table

--- client/src/app/club/board-game-table/board-game-table.component.ts
+++ client/src/app/club/board-game-table/board-game-table.component.ts
@@ -1,11 +1,11 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnChanges, Output } from '@angular/core';
 import { ButtonModule } from 'primeng/button';
 import { TableModule } from 'primeng/table';
 import { PipeModule } from '../../shared/pipes/pipe.module';
 import { CommonModule } from '@angular/common';
 import { Observable } from 'rxjs';
 import { StatsModel } from '../../shared/models/stats.model';
-import { BoardGameEntity } from 'libs/index';
+import { BoardGameEntity, PlayerEntity } from 'libs/index';
 
 @Component({
   selector: 'app-board-game-table',
@@ -14,12 +14,21 @@ import { BoardGameEntity } from 'libs/index';
   templateUrl: './board-game-table.component.html',
   styleUrl: './board-game-table.component.scss',
 })
-export class BoardGameTableComponent {
+export class BoardGameTableComponent implements OnChanges {
   @Input() boardGames$?: Observable<BoardGameEntity[]>;
+  @Input() players: PlayerEntity[] | null = null;
   @Input() canEdit = false;
   @Input() stats?: StatsModel;
 
   @Output() boardGameEdit = new EventEmitter<BoardGameEntity>();
+
+  WinCounts: {
+    [boardGameId: string]: {
+      playerId: string;
+      name: string;
+      count: number;
+    }[];
+  } = {};
 
   expandedRows = {};
   boardGameColumns = [
@@ -33,4 +42,39 @@ export class BoardGameTableComponent {
     { field: 'AverageScore', name: 'Average Score', sort: true, width: 0 },
     { field: 'AverageWinningScore', name: 'Average Winning Score', sort: true },
   ];
+
+  ngOnChanges(): void {
+    if (this.players) {
+      this.calculateWinCounts(this.players);
+    } else {
+      this.WinCounts = {};
+    }
+  }
+
+  calculateWinCounts(players: PlayerEntity[]) {
+    players.forEach((player) => {
+      player.Wins.forEach((win) => {
+        if (this.WinCounts[win.Game?.BoardGameId ?? ''] === undefined) {
+          this.WinCounts[win.Game?.BoardGameId ?? ''] = [];
+        } else {
+          // Continue
+        }
+
+        const row = this.WinCounts[win.Game?.BoardGameId ?? ''].find((x) => x.playerId === player.PlayerId);
+        if (row) {
+          row.count++;
+        } else {
+          this.WinCounts[win.Game?.BoardGameId ?? ''].push({
+            playerId: player.PlayerId ?? '',
+            name: player.Name ?? 'Unknown',
+            count: 1,
+          });
+        }
+      });
+    });
+
+    Object.values(this.WinCounts).forEach((count) => {
+      count.sort((a, b) => b.count - a.count || a.name.localeCompare(b.name));
+    });
+  }
 }
