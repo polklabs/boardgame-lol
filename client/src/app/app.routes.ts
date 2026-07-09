6a5413701a322fb1fcbe4dd5b036c03cf0d13dd9
Andrew Polk <polkandrew01@gmail.com>
2026-07-09 11:22:49 -0700

    Code cleanup

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -2,12 +2,10 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { ClubComponent } from './club/club.component';
-import { BoardGameComponent } from './board-game/board-game.component';
 import { PlayerComponent } from './player/player.component';
 
 export const routes: Routes = [
   { path: 'club/:id', component: ClubComponent },
-  { path: 'club/board-game', component: BoardGameComponent },
   { path: 'club/player', component: PlayerComponent },
   { path: 'home', component: HomeComponent },
   { path: '', redirectTo: '/home', pathMatch: 'full' },
