451bd5e0682fdff03b3176addf218befe5163903
Andrew Polk <polkandrew01@gmail.com>
2026-07-23 16:52:59 -0700

    Move shuffle to a helper file.

--- libs/src/utils/helper-utils.ts
+++ libs/src/utils/helper-utils.ts
@@ -64,3 +64,20 @@ export function ConvertListToDict<T>(list: T[], entityType: new (partial: Partia
 
   return toReturn;
 }
+
+export function shuffle<T>(array: T[]): T[] {
+  let currentIndex = array.length;
+  let randomIndex;
+
+  // While there remain elements to shuffle.
+  while (currentIndex != 0) {
+    // Pick a remaining element.
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+
+    // And swap it with the current element.
+    [array[currentIndex], array[randomIndex]] = [array[randomIndex], array[currentIndex]];
+  }
+
+  return array;
+}
