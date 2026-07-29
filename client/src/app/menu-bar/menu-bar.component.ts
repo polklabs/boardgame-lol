e353157613c4d40e862296ce23f1fdaa4e874563
Andrew Polk <polkandrew01@gmail.com>
2026-07-28 20:57:10 -0700

    Only render tabs in view. Auto collapse menu-bar

--- client/src/app/menu-bar/menu-bar.component.ts
+++ client/src/app/menu-bar/menu-bar.component.ts
@@ -1,5 +1,17 @@
 import { CommonModule } from '@angular/common';
-import { Component, EventEmitter, Input, OnDestroy, OnInit, Output, inject } from '@angular/core';
+import {
+  AfterViewInit,
+  ChangeDetectorRef,
+  Component,
+  ElementRef,
+  EventEmitter,
+  Input,
+  OnDestroy,
+  OnInit,
+  Output,
+  ViewChild,
+  inject,
+} from '@angular/core';
 import { ButtonModule } from 'primeng/button';
 import { SplitButtonModule } from 'primeng/splitbutton';
 import { Router } from '@angular/router';
@@ -10,6 +22,7 @@ import { MenuModule } from 'primeng/menu';
 import { LoginComponent } from '../login/login.component';
 import { PasswordUpdateComponent } from '../password-update/password-update.component';
 import { MenuItem } from 'primeng/api';
+import { debounce } from 'lodash-es';
 
 function actionToBtn(data: [string, string, EventEmitter<void>]) {
   return {
@@ -38,12 +51,15 @@ type Buttons =
   templateUrl: './menu-bar.component.html',
   styleUrl: './menu-bar.component.scss',
 })
-export class MenuBarComponent implements OnInit, OnDestroy {
+export class MenuBarComponent implements OnInit, OnDestroy, AfterViewInit {
+  @ViewChild('header', { static: true }) header!: ElementRef;
+
   private router = inject(Router);
   private apiService = inject(ApiService);
   private userService = inject(UserService);
+  private cdr = inject(ChangeDetectorRef);
 
-  @Input() page: 'home' | 'club' | 'player' | 'board-game' = 'home';
+  @Input() page: 'home' | 'club' = 'home';
 
   @Output() editClub = new EventEmitter();
   @Output() newClub = new EventEmitter();
@@ -58,6 +74,10 @@ export class MenuBarComponent implements OnInit, OnDestroy {
 
   @Output() editTags = new EventEmitter();
   @Output() editEvents = new EventEmitter();
+
+  collapseView = false;
+  fullViewWidth = 0;
+  collapseDebounce = debounce(this.calculateView.bind(this), 50);
 
   canEdit = false;
   loggedIn$: Observable<boolean> = of(false);
@@ -97,26 +117,22 @@ export class MenuBarComponent implements OnInit, OnDestroy {
   ];
 
   editClubItems = [
-    this.actions.clubEdit,
+    
     this.actions.gameAdd,
     this.actions.boardGameAdd,
     this.actions.playerAdd,
-    this.actions.tagEdit,
-    this.actions.eventEdit,
-  ];
-
-  editPlayerItems = [this.actions.playerEdit, this.actions.clubEdit, this.actions.tagEdit, this.actions.eventEdit];
-
-  editBoardGameItems = [
-    this.actions.boardGameEdit,
     this.actions.clubEdit,
     this.actions.tagEdit,
     this.actions.eventEdit,
   ];
+
+  editItems = [this.actions.clubEdit, this.actions.tagEdit, this.actions.eventEdit];
+  addItems = [this.actions.gameAdd, this.actions.boardGameAdd, this.actions.playerAdd];
 
   editHomeItems = [this.actions['clubAdd']];
 
   subscriptions = new Subscription();
+  observer = new ResizeObserver(() => {});
 
   ngOnInit() {
     if (this.page === 'home') {
@@ -136,8 +152,16 @@ export class MenuBarComponent implements OnInit, OnDestroy {
     this.loggedIn$ = this.userService.loggedIn$;
   }
 
+  ngAfterViewInit(): void {
+    this.observer = new ResizeObserver(() => {
+      this.collapseDebounce();
+    });
+    this.observer.observe(this.header.nativeElement);
+  }
+
   ngOnDestroy(): void {
     this.subscriptions.unsubscribe();
+    this.observer.disconnect();
   }
 
   navigateHome() {
@@ -146,5 +170,28 @@ export class MenuBarComponent implements OnInit, OnDestroy {
 
   username() {
     return this.userService.username;
+  }
+
+  calculateView() {
+    const width = this.header.nativeElement.clientWidth;
+    const scrollWidth = this.header.nativeElement.scrollWidth;
+    if (width < scrollWidth) {
+      this.collapseView = true;
+      this.fullViewWidth = width;
+    } else if (width < this.fullViewWidth) {
+      if (this.collapseView || width < scrollWidth) {
+        this.collapseView = true;
+      } else {
+        // Leave as is
+      }
+      this.fullViewWidth = width;
+    } else if (width > this.fullViewWidth) {
+      this.collapseView = false;
+      this.fullViewWidth = width;
+      setTimeout(() => this.calculateView(), 0);
+    } else {
+      // Keep
+    }
+    this.cdr.detectChanges();
   }
 }
