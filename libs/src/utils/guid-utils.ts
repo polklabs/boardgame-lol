1b3aa22fb031c1b8807b05ea5441009da1ed4d38
Andrew Polk <andrew@polklabs.com>
2026-08-16 10:23:26 -0700

    Use crypto uuid instead of separate package.

--- libs/src/utils/guid-utils.ts
+++ libs/src/utils/guid-utils.ts
@@ -1,10 +1,8 @@
-import { v4 as guid } from 'uuid';
-
 const guidRegex =
   /^(?<first>[a-fA-F\d]{8})-?(?<second>[a-fA-F\d]{4})-?(?<third>[a-fA-F\d]{4})-?(?<fourth>[a-fA-F\d]{4})-?(?<fifth>[a-fA-F\d]{12})$/gm;
 
 export function isGuid(guid: string | undefined | null): boolean {
-  if (!guid) {
+  if (!guid || (guid.length !== 36 && guid.length !== 32)) {
     return false;
   } else {
     return new RegExp(guidRegex).test(guid);
@@ -12,5 +10,5 @@ export function isGuid(guid: string | undefined | null): boolean {
 }
 
 export function newGuid(): string {
-  return guid();
+  return crypto.randomUUID().replaceAll('-', '');
 }
